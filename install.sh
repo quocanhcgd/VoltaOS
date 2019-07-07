5bb24d95847c5eb88fc6fab5486e688b907172db
Sacha Telgenhof Oude Koehorst <stelgenhof@gmail.com>
2019-07-07 17:33:32 +0900

    Corrected third party PHP repository.

--- install.sh
+++ install.sh
@@ -34,7 +34,7 @@ usermod -aG www-data $VOLTA_OS_USER
 usermod -aG sudo $VOLTA_OS_USER
 
 # Install PHP (and related)
-wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
-echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php7.3.list
+apt-get install software-properties-common
+add-apt-repository ppa:ondrej/php
 apt-get update
 apt-get install -y php7.3-cli php7.3-fpm php7.3-intl php7.3-json php7.3-mbstring php7.3-xml php7.3-curl php7.3-bcmath php7.3-zip php7.3-sqlite
