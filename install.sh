0e20feb09a4cb93467403a82d50bfec21d990ff3
Sacha Telgenhof Oude Koehorst <stelgenhof@gmail.com>
2019-07-07 17:37:39 +0900

    Add installation step for Composer

--- install.sh
+++ install.sh
@@ -38,3 +38,5 @@ apt-get install software-properties-common
 add-apt-repository ppa:ondrej/php
 apt-get update
 apt-get install -y php7.3-cli php7.3-fpm php7.3-intl php7.3-json php7.3-mbstring php7.3-xml php7.3-curl php7.3-bcmath php7.3-zip php7.3-sqlite
+
+curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
